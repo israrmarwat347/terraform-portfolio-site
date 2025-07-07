ffee19b1c7c93756dc860d97ce7d92c81012c925
Ubuntu <ubuntu@ip-172-31-10-28.us-east-2.compute.internal>
2025-07-07 02:31:31 +0000

    Initial commit - Terraform S3 portfolio project

---
+++ main.tf
@@ -0,0 +1,73 @@
+#create s3 bucket 
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = var.bucketname
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.mybucket.id
+  key    = "error.html"
+  source = "error.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid       = "PublicReadGetObject",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.mybucket.arn}/*"
+    }]
+  })
+}
+
+
+resource "aws_s3_bucket_website_configuration" "website" {
+	bucket = aws_s3_bucket.mybucket.id
+	index_document {
+	 suffix = "index.html"
+	}
+
+	error_document {
+	 key = "error.html"
+	}
+}
